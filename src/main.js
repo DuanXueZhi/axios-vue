cd7340265f06cd50545fd3afbf95bea7dc72e6ef
Kitai_cnki_vm1 <794465732@qq.com>
2020-08-06 17:37:43 +0800

    element-ui

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import Http from './service/http'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 Vue.prototype.$Http = Http // http挂载vue实例
 
