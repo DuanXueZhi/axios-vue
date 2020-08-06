1213e63f5f8947c274e4e96efcb13c7593c145b4
DuanXueZhi <38554820+DuanXueZhi@users.noreply.github.com>
2020-08-06 23:25:45 +0800

    全局使用

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,9 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <back-top>
+      <router-view/>
+    </back-top>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import Http from './service/http'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import './globalComponents'
 
 Vue.use(ElementUI)
 Vue.config.productionTip = false
